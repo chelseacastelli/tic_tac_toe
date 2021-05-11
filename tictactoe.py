ce6ea5cb0587825baed5d45abbb5c64e95c20862
chelseacastelli <chel.castel@aol.com>
2021-05-11 16:03:27 -0400

    implement score keeping functionality and ability to play again

--- tictactoe.py
+++ tictactoe.py
@@ -5,8 +5,6 @@ from pygame.locals import *
 
 pygame.init()
 
-
-## Part 1:
 # Create blank game window
 screen_width = 300;
 screen_height = 300;
@@ -21,10 +19,19 @@ markers = []
 clicked = False
 pos = []
 player = 1
+winner = 0
+game_over = False
 
 # Define colors (marker colors)
 green = (0, 255, 0)
 red = (255, 0, 0)
+blue = (0, 0, 255)
+
+# Define font
+font = pygame.font.Font(None, 40)
+
+# Create rectangle
+again_rect = Rect(screen_width // 2 - 80, screen_height // 2, 160, 50)
 
 # Create grid and setup the grid list
 def draw_grid():
@@ -40,19 +47,79 @@ for i in range(3):
     markers.append(row)
 
 
+# Setup function for drawing markers onto the grid
 def draw_markers():
     x_pos = 0
     for x in markers:
         y_pos = 0
         for y in x:
             if y == 1:
-                pygame.draw.line(screen, green, (x_pos * 100 + 15, y_pos * 100 + 15), (x_pos * 100 + 85, y_pos * 100 + 85), line_width) 
+                pygame.draw.line(screen, green, (x_pos * 100 + 15, y_pos * 100 + 15), (x_pos * 100 + 85, y_pos * 100 + 85), line_width)
                 pygame.draw.line(screen, green, (x_pos * 100 + 15, y_pos * 100 + 85), (x_pos * 100 + 85, y_pos * 100 + 15), line_width)
             if y == -1:
                 pygame.draw.circle(screen, red, (x_pos * 100 + 50, y_pos * 100 + 50), 38, line_width)
-            
+
             y_pos += 1
         x_pos += 1
+
+
+# Create check_winner() including checks for winning combos
+def check_winner():
+
+    global winner
+    global game_over
+    y_pos = 0
+
+    for x in markers:
+        # Check columns
+        if sum(x) == 3:
+            # Check if winner is player 1
+            winner = 1
+            game_over = True
+        if sum(x) == -3:
+            # Check if winner is player 2
+            winner = 2
+            game_over = True
+
+        # Check rows
+        if markers[0][y_pos] + markers[1][y_pos] + markers[2][y_pos] == 3:
+            winner = 1
+            game_over = True
+        if markers[0][y_pos] + markers[1][y_pos] + markers[2][y_pos] == -3:
+            winner = 2
+            game_over = True
+
+    # Check diagonal
+    if markers[0][0] + markers[1][1] + markers[2][2] == 3 or markers[2][0] + markers[1][1] + markers[0][2] == 3:
+        winner = 1
+        game_over = True
+    if markers[0][0] + markers[1][1] + markers[2][2] == -3 or markers[2][0] + markers[1][1] + markers[0][2] == -3:
+        winner = 2
+        game_over = True
+
+
+def draw_winner(winner):
+    win_text = f'Player {winner} wins!'
+    win_img = font.render(win_text, True, blue)
+    pygame.draw.rect(screen, green, (screen_width // 2 - 100, screen_height // 2 - 60, 200, 50))
+    screen.blit(win_img, (screen_width // 2 - 100, screen_height // 2 - 50))
+
+    again_text = 'Play Again?'
+    again_img = font.render(again_text, True, blue)
+    pygame.draw.rect(screen, green, again_rect)
+    screen.blit(again_img, (screen_width // 2 - 80, screen_height // 2 + 10))
+
+#
+# def reset_game():
+#     markers = []
+#     pos = []
+#     player = 1
+#     winner = 0
+#     game_over = False
+#
+#     for x in range(3):
+#         row = [0] * 3
+#         markers.append(row)
 
 # Setup loop with exit event handler
 run = True
@@ -66,35 +133,47 @@ while run:
         # If we click exit button on window
         if event.type == pygame.QUIT:
             run = False
-        
-        # Mouse click
+
+        # Create event handler for clicking on a cell
+        if game_over == 0:
+            if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
+                clicked = True
+            if event.type == pygame.MOUSEBUTTONUP and clicked == True:
+                clicked = False
+                # Get (x,y) of mouse when clicked
+                pos = pygame.mouse.get_pos()
+                cell_x = pos[0]
+                cell_y = pos[1]
+
+                # Check if cell is empty and place a marker
+                if markers[cell_x // 100][cell_y // 100] == 0:
+                    markers[cell_x // 100][cell_y // 100] = player
+                    player *= -1
+                    check_winner()
+
+    if game_over == True:
+        draw_winner(winner)
+        # Check for mouse click to see if user clicked on Play Again
         if event.type == pygame.MOUSEBUTTONDOWN and clicked == False:
             clicked = True
         if event.type == pygame.MOUSEBUTTONUP and clicked == True:
-            click = False
-            # Get (x,y) of mouse when clicked
+            clicked = False
             pos = pygame.mouse.get_pos()
-            cell_x = pos[0]
-            cell_y = pos[1]
 
-            if markers[cell_x // 100][cell_y // 100] == 0:
-                markers[cell_x // 100][cell_y // 100] = player
-                player *= -1
+            # If on click, mouse is in bounds of Play Again rectangle..
+            if again_rect.collidepoint(pos):
+                # then reset game
+                markers = []
+                pos = []
+                player = 1
+                winner = 0
+                game_over = False
+
+                for x in range(3):
+                    row = [0] * 3
+                    markers.append(row)
+
 
     pygame.display.update()
-        
+
 pygame.quit()
-
-
-
-## Part 2:
-# Create event handler for clicking on a cell
-# Check if cell is empty and place a marker
-# Setup function for drawing markers onto the grid
-
-## Part 3:
-# Create check_winner() including checks for winning combos
-
-# Create event handler for post game
-
-# Reset game
