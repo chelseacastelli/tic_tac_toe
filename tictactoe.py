fbd96399cce3b15bbb79d02ffc5d32e4df84cc80
chelseacastelli <chelsea.castelli@students.makeschool.com>
2021-05-11 13:11:04 -0400

    create blank game board

--- tictactoe.py
+++ tictactoe.py
@@ -1,10 +1,44 @@
 
 #import modules
+import pygame
+from pygame.locals import *
+
+pygame.init()
 
 ## Part 1:
 # Create blank game window
-# Setup loop with exit event handler
+screen_width = 300;
+screen_height = 300;
+
+# Displays screen
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('TicTacToe')
+
+line_width = 6
+
 # Create grid and setup the grid list
+def draw_grid():
+    bg = (255, 255, 200)
+    grid = (50, 50, 50)
+    screen.fill(bg)
+    for x in range(1,3):
+        pygame.draw.line(screen, grid, (0, x * 100), (screen_width, x * 100), line_width)
+        pygame.draw.line(screen, grid, (x * 100, 0), (x * 100, screen_height), line_width)
+
+# Setup loop with exit event handler
+run = True
+while run:
+
+    draw_grid()
+
+    for event in pygame.event.get():
+        # If we click exit button on window
+        if event.type == pygame.QUIT:
+            run = False
+
+    pygame.display.update()
+        
+pygame.quit()
 
 ## Part 2:
 # Create event handler for clicking on a cell
