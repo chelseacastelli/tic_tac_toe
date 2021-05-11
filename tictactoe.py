05dc4b8a6f6b75f3aa9298e27b429dcafc70315c
chelseacastelli <chelsea.castelli@students.makeschool.com>
2021-05-11 12:32:01 -0400

    create empty project with checklist

---
+++ tictactoe.py
@@ -0,0 +1,19 @@
+
+#import modules
+
+## Part 1:
+# Create blank game window
+# Setup loop with exit event handler
+# Create grid and setup the grid list
+
+## Part 2:
+# Create event handler for clicking on a cell
+# Check if cell is empty and place a marker
+# Setup function for drawing markers onto the grid
+
+## Part 3:
+# Create check_winner() including checks for winning combos
+
+# Create event handler for post game
+
+# Reset game
